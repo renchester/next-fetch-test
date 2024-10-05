d41e4958e1284b29219f9df9591bb19f93911715
Renchester Ramos <renchesterjramos@gmail.com>
2024-10-05 20:51:38 +0800

    Add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import styles from './Loading.module.css';
+
+function Loading() {
+  return (
+    <div className={styles.ellipsis}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+export default Loading;
