a474da5ee90ee5caffc0d083809a0686f52abc57
Renchester Ramos <renchesterjramos@gmail.com>
2024-10-05 18:30:15 +0800

    Add initial data request from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import UsersList from '@/components/UsersList';
+import { API_URL } from '@/config';
+import { type ResponseData } from '@/types';
 import styles from './page.module.css';
 
-export default function Home() {
-  return <div className={styles.page}>Hello World!</div>;
+export default async function Home() {
+  // Fetch initial data from API
+  const res = await fetch(`${API_URL}?page=1`);
+  const data: ResponseData = await res.json();
+  const users = data.data;
+
+  return (
+    <div className={styles.page}>
+      <UsersList initialUsers={users} />
+    </div>
+  );
 }
